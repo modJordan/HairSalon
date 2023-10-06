e17fa445566212091d88b0d27a941fceb44983e7
modjordan <mod.jordan@icloud.com>
2023-10-06 12:48:07 -0700

    completed Model logic and controller logic.

--- HairSalon.Solution/HairSalon/Controllers/StylistsController.cs
+++ HairSalon.Solution/HairSalon/Controllers/StylistsController.cs
@@ -3,3 +3,40 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+  }
+}
--- HairSalon.Solution/HairSalon/Models/Stylist.cs
+++ HairSalon.Solution/HairSalon/Models/Stylist.cs
@@ -6,7 +6,17 @@ namespace HairSalon.Models
   {
     public int StylistId { get; set; }
     public string Name { get; set; }
-    public string Specialty { get; set; }
+    public Specialty Specialty { get; set; }
     public List<Client> Clients { get; set; }
+  }
+
+  public enum Specialty
+  {
+    Haircut = 1,
+    Color = 2,
+    Perm = 3,
+    Style = 4,
+    CurlExpert = 5,
+    Bridal = 6
   }
 }
